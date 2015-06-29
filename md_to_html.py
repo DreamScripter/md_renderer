cb204754780fc252d99efe74d593dc14ee9b0600
DreamScripter <DreamScripter@hushmail.com>
2015-06-29 11:59:42 +0200

    Copy styles.css to every directory

--- md_to_html.py
+++ md_to_html.py
@@ -12,17 +12,16 @@ html_footer = '</div></body></html>'
 os.chdir(base_path)
 
 if not os.path.exists(base_path_html):
-    print("No existe", base_path_html, "creando ...")
     os.mkdir(base_path_html)
-
-shutil.copyfile(os.path.join(project_path, 'style.css'), os.path.join(base_path_html, 'style.css'))
 
 for path, dirs, files in os.walk("."):
     dir_html = os.path.join(base_path_html, path)
     if not os.path.exists(dir_html):
-        print("no existe:", dir_html, " creando ...")
         os.mkdir(dir_html)
-
+        
+    # copy the css to every directory
+    shutil.copyfile(os.path.join(project_path, 'style.css'), os.path.join(dir_html, 'style.css'))
+       
     for filename in files:
         if filename.endswith(('md')):
             file_without_ext, ext = os.path.splitext(filename)
@@ -38,3 +37,4 @@ for path, dirs, files in os.walk("."):
             # add footer to html file
             with open(rendered_file, 'a') as f:
                 f.write(html_footer.rstrip('\r\n') + '\n')
+
